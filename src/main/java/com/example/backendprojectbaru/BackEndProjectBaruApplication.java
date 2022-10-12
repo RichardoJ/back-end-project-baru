4e74d3c4d319760e8661260f8ce5834ffe204e8b
RichardoJ <79075790+RichardoJ@users.noreply.github.com>
2022-10-12 20:11:55 +0200

    testing

--- src/main/java/com/example/backendprojectbaru/BackEndProjectBaruApplication.java
+++ src/main/java/com/example/backendprojectbaru/BackEndProjectBaruApplication.java
@@ -2,8 +2,10 @@ package com.example.backendprojectbaru;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.*;
 
 @SpringBootApplication
+@ImportResource({"classpath*:application-context.xml"})
 public class BackEndProjectBaruApplication {
 
 	public static void main(String[] args) {
